2b3d3711c8c0763a85cce3be7b41b929160d7fc7
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-12-08 16:44:05 -0800

    created form to work with custom hook to push data into graphql api

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,10 +1,50 @@
+import useForm from '../lib/useForm';
+
 export default function CreateProduct() {
+  const { inputs, handleChange, clearForm } = useForm({
+    image: '',
+    name: '',
+    price: 0,
+    description: '',
+  });
   return (
     <form>
       <label htmlFor="name">
         Name
-        <input type="text" id="name" name="name" placeholder="Name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          value={inputs.name}
+          onChange={handleChange}
+        />
       </label>
+      <label htmlFor="price">
+        Price
+        <input
+          type="number"
+          id="price"
+          name="price"
+          placeholder="price"
+          value={inputs.price}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="description">
+        Description
+        <textarea
+          id="description"
+          name="description"
+          placeholder="Description"
+          value={inputs.description}
+          onChange={handleChange}
+        />
+      </label>
+
+      <button type="button" onClick={clearForm}>
+        Clear Form
+      </button>
     </form>
   );
 }
