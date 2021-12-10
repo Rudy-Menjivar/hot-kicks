3ed749e8354e37d6944809e20f901632c86c4b68
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-12-09 16:38:27 -0800

    applied dynamic variables into graphql mutation to create products from front end to backend

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,5 +1,33 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+import DisplayError from './ErrorMessage';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # Variables that are getting passed in with their type
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        photo: { create: { image: $image, altText: $name } }
+        status: "AVAILABLE"
+      }
+    ) {
+      id
+      name
+      price
+      description
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm } = useForm({
@@ -8,12 +36,33 @@ export default function CreateProduct() {
     price: 0,
     description: '',
   });
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
   return (
-    <Form>
-      <fieldset>
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        console.log(inputs);
+        // Submit input fields to backend
+        await createProduct();
+        clearForm();
+      }}
+    >
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
-          <input type="file" id="image" name="image" onChange={handleChange} />
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
         </label>
         <label htmlFor="name">
           Name
@@ -32,7 +81,7 @@ export default function CreateProduct() {
             type="number"
             id="price"
             name="price"
-            placeholder="price"
+            placeholder="Price in cents"
             value={inputs.price}
             onChange={handleChange}
           />
