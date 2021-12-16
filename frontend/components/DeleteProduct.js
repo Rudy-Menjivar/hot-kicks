eb0bad53509a3e6e0dbe64ec8e86adc2e9e6627e
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-12-15 17:30:12 -0800

    added delete button with delete mutation and cache update after successful delete req

---
+++ frontend/components/DeleteProduct.js
@@ -0,0 +1,41 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+// remove deleted item from cache of all prod query
+function update(cache, payload) {
+  console.log(payload);
+  cache.evict(cache.identify(payload.data.deleteProduct));
+}
+
+export default function DeleteProduct({ id, children }) {
+  const [deleteProduct, { loading, error }] = useMutation(
+    DELETE_PRODUCT_MUTATION,
+    {
+      variables: { id },
+      update,
+    }
+  );
+  return (
+    <button
+      type="button"
+      disabled={loading}
+      onClick={() => {
+        if (confirm('Are you sure you want to delete this item?')) {
+          // Delete product after confirmation is true
+          deleteProduct().catch((err) => alert(err.message));
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+}
