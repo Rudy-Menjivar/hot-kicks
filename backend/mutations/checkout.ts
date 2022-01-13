a95261eaa6453a3af9037fb1a0502df54f2d676f
Rudy Menjivar <rudy.menjivar@gmail.com>
2022-01-12 18:52:56 -0800

    created custom checkout mutation with stripe. Added logic to: ensuring a user is signed in, querying the current user, and calc total price of the order

---
+++ backend/mutations/checkout.ts
@@ -0,0 +1,62 @@
+import { KeystoneContext } from '@keystone-next/types';
+import {
+  CartItemCreateInput,
+  OrderCreateInput,
+} from '../.keystone/schema-types';
+
+/* eslint-disable */
+interface Arguments {
+  token: string
+}
+
+const graphql = String.raw;
+
+async function checkout(
+  root: any,
+  { token }: Arguments,
+  context: KeystoneContext
+): Promise<OrderCreateInput> {
+  // 1. Make sure they're signed in
+  const userId = context.session.itemId;
+  if (!userId) {
+    throw new Error('You must be signed in to create an order!')
+  }
+  // 2. Query the current user
+  const user = await context.lists.User.findOne({
+    where: { id: userId },
+    resolveFields: graphql`
+      id
+      name
+      email
+      cart {
+        id
+        quantity
+        product {
+          name
+          price
+          description
+          id
+          photo {
+            id
+            image {
+              id
+              publicUrlTransformed
+            }
+          }
+        }
+      }
+    `,
+  });
+  console.dir(user, { depth: null })
+  // 3. Calculate the total price for their order
+  const cartItems = user.cart.filter(cartItem => cartItem.product);
+  const amount = cartItems.reduce(function(tally: number, cartItem: CartItemCreateInput) {
+    return tally + cartItem.quantity * cartItem.product.price;
+  }, 0);
+  console.log(amount);
+  // 4. Create the charge with the stripe lib
+  // 5. Convert the cartItems to OrderItems
+  // 6. Create the order and return it
+}
+
+export default checkout;
