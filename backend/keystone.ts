34a2c111c9e392f3cf569ac820d90f333933066e
Rudy Menjivar <rudy.menjivar@gmail.com>
2022-01-12 16:01:21 -0800

    created order and orderitem data types

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -4,6 +4,8 @@ import {
   withItemData,
   statelessSessions,
 } from '@keystone-next/keystone/session';
+import { OrderItem } from './schemas/OrderItem';
+import { Order } from './schemas/Order';
 import { CartItem } from './schemas/CartItems';
 import { ProductImage } from './schemas/ProductImage';
 import { Product } from './schemas/Product';
@@ -61,6 +63,8 @@ export default withAuth(
       Product,
       ProductImage,
       CartItem,
+      OrderItem,
+      Order,
     }),
     extendGraphqlSchema,
     ui: {
--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -1,22 +1,22 @@
 import { list } from '@keystone-next/keystone/schema';
 import { text, password, relationship } from '@keystone-next/fields';
 
-// named export
 export const User = list({
-    // access:
-    // ui:
-    fields: {
-        name: text({ isRequired: true }),
-        email: text({ isRequired: true, isUnique: true }),
-        password: password(),
-        cart: relationship({
-            ref: 'CartItem.user',
-            many: true,
-            ui: {
-                createView: { fieldMode: 'hidden' },
-                itemView: { fieldMode: 'read' },
-            },
-        }),
-        // TODO: add roles, cart and orders
-    },
+  // access:
+  // ui:
+  fields: {
+    name: text({ isRequired: true }),
+    email: text({ isRequired: true, isUnique: true }),
+    password: password(),
+    cart: relationship({
+      ref: 'CartItem.user',
+      many: true,
+      ui: {
+        createView: { fieldMode: 'hidden' },
+        itemView: { fieldMode: 'read' },
+      },
+    }),
+    orders: relationship({ ref: 'Order.user', many: true }),
+    // TODO: add roles
+  },
 });
