56865a58a8123eb35f5acf6a8b47d9b081cca36c
Rudy Menjivar <rudy.menjivar@gmail.com>
2022-01-19 14:50:13 -0800

    restricted role create and update access. Also restricted read, update and delete from User schema

--- backend/access.ts
+++ backend/access.ts
@@ -66,4 +66,15 @@ export const rules = {
     // They should only see available products (based on the status field)
     return { status: 'AVAILABLE' };
   },
+  canManageUsers({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    // 1. Do they have the permission of canManageCart
+    if (permissions.canManageUsers({ session })) {
+      return true;
+    }
+    // Otherwise they can only update themselves
+    return { id: session.itemId };
+  },
 };
