ff87b73ade75b9f120dc99a09f0f734085b23c73
Rudy Menjivar <rudy.menjivar@gmail.com>
2022-01-20 23:29:19 -0800

    including snapshot testing for Product component

--- frontend/__tests__/Product.test.js
+++ frontend/__tests__/Product.test.js
@@ -12,6 +12,19 @@ describe('<Product/>', () => {
         <Product product={product} />
       </MockedProvider>
     );
-    debug();
+    const priceTag = screen.getByText('$50');
+    expect(priceTag).toBeInTheDocument();
+    const link = container.querySelector('a');
+    expect(link).toHaveAttribute('href', '/product/abc123');
+    expect(link).toHaveTextContent(product.name);
+  });
+
+  it('Renders and matches the snapshot', () => {
+    const { container, debug } = render(
+      <MockedProvider>
+        <Product product={product} />
+      </MockedProvider>
+    );
+    expect(container).toMatchSnapshot();
   });
 });
