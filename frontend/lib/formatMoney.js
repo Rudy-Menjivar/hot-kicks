86425292e4e21d8aa195700743bd485397035379
Rudy Menjivar <rudy.menjivar@gmail.com>
2022-01-20 22:52:51 -0800

    changed min fraction digits to 2 and corrected a typo on if statement

--- frontend/lib/formatMoney.js
+++ frontend/lib/formatMoney.js
@@ -2,12 +2,12 @@ export default function formatMoney(amount = 0) {
   const options = {
     style: 'currency',
     currency: 'USD',
-    minimumFractionDigits: 0,
+    minimumFractionDigits: 2,
   };
 
   // if whole dollar then eliminate cents
   if (amount % 100 === 0) {
-    options.minimumFranctionDigits = 0;
+    options.minimumFractionDigits = 0;
   }
 
   const formatter = Intl.NumberFormat('en-US', options);
