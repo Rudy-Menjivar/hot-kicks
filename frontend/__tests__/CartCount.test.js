3f7eecc2c368382e0fe97ba0563bd52a45d10831
Rudy Menjivar <rudy.menjivar@gmail.com>
2022-01-21 14:17:07 -0800

    re-rendered props with react test library

--- frontend/__tests__/CartCount.test.js
+++ frontend/__tests__/CartCount.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import CartCount from '../components/CartCount';
 
 describe('<CartCount>', () => {
@@ -9,14 +9,16 @@ describe('<CartCount>', () => {
     const { container } = render(<CartCount count={12} />);
     expect(container).toMatchSnapshot();
   });
-  it('Updates via props', () => {
+  it('Updates via props', async () => {
     const { container, rerender, debug } = render(<CartCount count={13} />);
-    // ? Vanillay JS & fancier custom matcher
+    // ? Vanilla JS & fancier custom matcher
     expect(container.textContent).toBe('13');
-    expect(container).toHaveTextContent('13');
-    debug();
     // ? Update the props
-    render(<CartCount count="14" />);
-    debug();
+    rerender(<CartCount count="14" />);
+    // ? Tests if duplicated animation is working
+    expect(container).toHaveTextContent('1413');
+    // ? Built in findBy utility method keeps looking until query times out
+    await screen.findByText('14');
+    expect(container).toMatchSnapshot();
   });
 });
