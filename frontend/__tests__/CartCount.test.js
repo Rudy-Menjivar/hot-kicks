73af22ce4a3f630cf0dd340fe0be072607d476bf
Rudy Menjivar <rudy.menjivar@gmail.com>
2022-01-21 12:51:47 -0800

    updated the props for testing

--- frontend/__tests__/CartCount.test.js
+++ frontend/__tests__/CartCount.test.js
@@ -6,8 +6,17 @@ describe('<CartCount>', () => {
     render(<CartCount count={10} />);
   });
   it('Matches Snapshot', () => {
-    const { container, debug } = render(<CartCount count={12} />);
+    const { container } = render(<CartCount count={12} />);
     expect(container).toMatchSnapshot();
+  });
+  it('Updates via props', () => {
+    const { container, rerender, debug } = render(<CartCount count={13} />);
+    // ? Vanillay JS & fancier custom matcher
+    expect(container.textContent).toBe('13');
+    expect(container).toHaveTextContent('13');
+    debug();
+    // ? Update the props
+    render(<CartCount count="14" />);
     debug();
   });
 });
