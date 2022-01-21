84a3c483d321dfe28a5c59bed6e7eb5bb61a0de1
Rudy Menjivar <rudy.menjivar@gmail.com>
2022-01-21 15:07:06 -0800

    added nav mock test when signed out to expect correct link attributes

---
+++ frontend/__tests__/Nav.test.js
@@ -0,0 +1,54 @@
+import { render, screen } from '@testing-library/react';
+import { MockedProvider } from '@apollo/react-testing';
+import Nav from '../components/Nav';
+import { CURRENT_USER_QUERY } from '../components/User';
+import { fakeUser, fakeCartItem } from '../lib/testUtils';
+import { CartStateProvider } from '../lib/cartState';
+
+// ? Mocks for logged out, logged in, and logged in with cart items
+const notSignedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { authenticatedItem: null } },
+  },
+];
+
+const signedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { authenticatedItem: fakeUser() } },
+  },
+];
+
+const signedInMocksWithCartItems = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: {
+      data: {
+        authenticatedItem: fakeUser({
+          cart: [fakeCartItem()],
+        }),
+      },
+    },
+  },
+];
+
+describe('<Nav/>', () => {
+  it('Renders with a minimal nav when signed out', () => {
+    const { container, debug } = render(
+      <CartStateProvider>
+        <MockedProvider mocks={notSignedInMocks}>
+          <Nav />
+        </MockedProvider>
+      </CartStateProvider>
+    );
+    expect(container).toHaveTextContent('Sign In');
+    expect(container).toMatchSnapshot();
+    const link = screen.getByText('Sign In');
+    screen.debug(link);
+    expect(link).toHaveAttribute('href', '/signin');
+    const productsLink = screen.getByText('Products');
+    expect(productsLink).toBeInTheDocument();
+    expect(productsLink).toHaveAttribute('href', '/products');
+  });
+});
